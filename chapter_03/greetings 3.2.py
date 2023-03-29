6f220739bbab7303eea267f5aa48dc48f835f2ec
Khuzaima Tariq <tariqmahmood@yandex.com>
2023-03-29 13:22:18 +0500

    Solved Exercise

---
+++ chapter_03/greetings 3.2.py
@@ -0,0 +1,6 @@
+friends = ['imran', 'asad', 'waqas', 'jahanzaib', 'shoaib']
+print(f"Hello, Mr. {friends[0].title()}")
+print(f"Hello, Mr. {friends[1].title()}")
+print(f"Hello, Mr. {friends[2].title()}")
+print(f"Hello, Mr. {friends[3].title()}")
+print(f"Hello, Mr. {friends[4].title()}")
---
+++ chapter_03/names 3.1.py
@@ -0,0 +1,6 @@
+friends = ['imran', 'asad', 'waqas', 'jahanzaib', 'shoaib']
+print(friends[0].title())
+print(friends[1].title())
+print(friends[2].title())
+print(friends[3].title())
+print(friends[4].title())
