9d0884547005edfe752a7874b0e7ff7e40987213
Khadija <khadija@scholar.tu.edu.pk>
2023-04-07 14:34:54 +0500

    solved exercise

---
+++ chapter_04/try it yourself4.13.py
@@ -0,0 +1,11 @@
+menu =('flafil', 'barbosa', 'karhai', 'acchar', 'chicken handi')
+print("today's menu is:")
+for food in menu:
+    print(food)
+#menu[0] = 'bbq'
+#menu[1] = 'trifal'
+#
+menu =('bbq', 'trifal', 'karhai', 'acchar', 'chicken handi')
+print("\nrevised menu is:")
+for food in menu:
+    print(food)
