22d8de26501ac8564a5d1f592b18033db278e23e
Khadija <khadija@scholar.tu.edu.pk>
2023-04-12 23:27:54 +0500

    solved exercise

---
+++ chapter_07/try it yourself7.4.py
@@ -0,0 +1,10 @@
+prompt = "enter topping that you want to add to your pizza"
+prompt += "\nenter 'quit' to end the program "
+pizza_topping = ""
+while pizza_topping != 'quit':
+    pizza_topping = input(prompt)
+    print(pizza_topping)
+print("we will add this topping to your pizza")
+
+
+ 
