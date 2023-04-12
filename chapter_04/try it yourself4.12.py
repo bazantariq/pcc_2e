189809fe1bd45136f77766d67bd3698a1450f847
Khadija <khadija@scholar.tu.edu.pk>
2023-04-12 23:35:52 +0500

    solved exercise

---
+++ chapter_04/try it yourself4.12.py
@@ -0,0 +1,8 @@
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friends_food = my_foods[:]
+print("My favourite foods are:")
+for food in my_foods:
+    print(food)
+print("\nMy friends favourite foods are:")
+for food in friends_food:
+    print(food)
