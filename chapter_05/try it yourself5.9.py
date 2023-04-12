9c4941c243f1aa9819b34010aa34c14e7c314255
Khadija <khadija@scholar.tu.edu.pk>
2023-04-12 20:32:57 +0500

    solved exercise

--- chapter_05/try it yourself5.9.py
+++ chapter_05/try it yourself5.9.py
@@ -1,6 +1,11 @@
 user_names = ['admin', 'Jhon', 'Ali', 'Sana', 'Ahamad', 'Abbas']
 if user_names:
+    print(user_names)
+
+user_names.clear()
+
+if user_names:
     for eachuser in user_names:
-        print(eachuser)
-
-
+            print("list is not empty")
+else:
+    print("we need to find some users")
