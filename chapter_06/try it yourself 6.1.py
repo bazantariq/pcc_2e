3e0f2d96c89303f435fe46f152bad7e1cbc23e96
Khadija <khadija@scholar.tu.edu.pk>
2023-05-25 22:52:03 +0500

    solved exercise

---
+++ chapter_06/try it yourself 6.1.py
@@ -0,0 +1,11 @@
+person = {
+    "first name": "Muaaz",
+    "last name": "Khalid",
+    "age": "50",
+    "city": "Bhakkar"
+}
+print(person)
+print(person["first name"])
+print(person["last name"])
+print(person["age"])
+print(person["city"])
---
+++ chapter_06/try it yourself 6.2.py
@@ -0,0 +1,10 @@
+favourite_number = {
+    "Ali": "65",
+    "Ahmad": "89",
+    "Sana": "24",
+    "Sara": "45",
+    "Saba": "34"
+}
+print(favourite_number)
+for name, value in favourite_number.items():
+    print(name + "'s favourite number: " + value)
