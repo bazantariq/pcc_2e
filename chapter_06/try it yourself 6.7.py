2875cd790e565cff2351e591948c52c815957c11
Khadija <khadija@scholar.tu.edu.pk>
2023-06-06 13:30:01 +0500

    solved exercise

---
+++ chapter_06/try it yourself 6.7.py
@@ -0,0 +1,23 @@
+person_0 = {
+    "first name": "Muaaz",
+    "last name": "Khalid",
+    "age": "50",
+    "city": "Bhakkar"
+}
+person_1 = {
+    "first name": "Tasbiha",
+    "last name": "Khalid",
+    "age": "16",
+    "city": "Bhakkar"
+
+}
+person_2 = {
+    "first name": "Sabir",
+    "last name": "Hassan",
+    "age": "26",
+    "city": "Bhakkar"
+
+}
+people = [person_0, person_1, person_2]
+for each in people:
+    print(each)
